f658f06dc2a06fee20cd9f2745ed631b3b81abb8
cordelia0625 <122579884+cordelia0625@users.noreply.github.com>
2023-03-07 14:44:37 -0800

    Add files via upload
    HW 3 Submission

---
+++ hw3/app.R
@@ -0,0 +1,123 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(datasets)
+library(tidyverse)
+library(lubridate)
+
+
+
+
+#load data
+mydata <- read_rds("D:\\Desktop\\203b-hw\\mimiciv_shiny\\icu_cohort.RDS")
+
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("ICU Cohort Data Exploration"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("var", label = "Choose a variable for the bar graph",
+                  choice = list(
+                    "Ethnicity" = "ethnicity",
+                    "Language" = "language",
+                    "Insurance" = "insurance",
+                    "Marital status" = "marital_status",
+                    "Gender" = "gender",
+                    "First Care Unit" = "first_careunit"
+
+                  )),
+      selectInput("m", label = "Choose a chart or lab measurement",
+                  choice = list(
+                    "Creatinine" = "item_50912",
+                    "Potassium" = "item_50971",
+                    "Sodium" = "item_50983",
+                    "Chloride" = "item_50902",
+                    "Bicarbonate" = "item_50882",
+                    "Hematocrit" = "item_51221",
+                    "White Blood Cell" = "item_51301",
+                    "Glucose" = "item_50931",
+                    "Magnesium" = "item_50960",
+                    "Heart Rate" = "item_220045",
+                    "Mean Non-invasive Blood Pressure" = "item_220181",
+                    "Systolic Non-invasive Blood Pressure" = "item_220179",
+                    "Body Temperature in Fahrenheit" = "item_223761",
+                    "Respiratory Rate" = "item_220210"
+                    
+                  )),
+   
+       
+                  
+      
+      
+    ),   
+    
+    mainPanel(
+      tabsetPanel(type = "tabs",
+                  tabPanel("Bar Graph for 30 day mortality", 
+                           plotOutput("myplot1")),
+                  tabPanel("Chart and Lab measurements", 
+                           plotOutput("myplot2")),
+                  tabPanel("Summary", tableOutput("table"))
+      )
+    )
+  )
+)
+
+
+
+
+
+# Define server
+server <- function(input, output) {
+  output$myplot1 <- renderPlot({
+
+    # Create a histogram using ggplot2
+    mydata %>%
+      ggplot() +
+      geom_bar(mapping =
+                 aes_string(x = "thirty_day_mort", fill = input$var),
+               position = "fill") +
+      labs(title = "Bar graph of thirty day mortality percentage")
+  })
+  
+  
+  output$myplot2 <- renderPlot ({
+  
+    # m_list <- list(input$m)
+    mydata %>% 
+      ggplot(aes_string(x = input$m)) + 
+      geom_boxplot() + 
+      labs(x = input$m, title = "Distribution of selected Lab or Chart event") 
+  
+  })
+  
+  
+  
+  
+  
+  output$table <- renderPrint({
+    summary(mydata[input$m])
+    
+
+  })
+    
+    
+  }
+# Run the application 
+shinyApp(ui = ui, server = server)
+
